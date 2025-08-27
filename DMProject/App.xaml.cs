fbddeea9bbacefe7ebe59969ad912ae3a040dd6c
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2025-08-27 20:58:01 +0330

    Add project files.

---
+++ DMProject/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace DMProject
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
