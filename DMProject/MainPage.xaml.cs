b170e6a4605baa548587fee646cacb7b6c57ee77
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2025-09-01 11:12:15 +0330

    feat: add functional ChangeQuestionNo and pasteTestCase buttons

--- DMProject/MainPage.xaml.cs
+++ DMProject/MainPage.xaml.cs
@@ -2,8 +2,8 @@
 {
     public partial class MainPage : ContentPage
     {
-        int isOutputVisible; // for managing the output box visibility
-        int questionNo = 1; // for managing the question number
+        int isOutputVisible { get; set; } // for managing the output box visibility
+        int questionNo { get; set; } = 1; // for managing the question number
         public MainPage() 
 
         {
@@ -12,8 +12,8 @@
 
         private void OnSendClicked(object sender, EventArgs e)
         {
-            string testCase1 = "4 2\nShiraz\nTehran\nIsfahan\nMashhad\nShiraz Tehran\nMashhad Isfahan\nMashhad";
-            string testCase2 = "5 4\nA\nB\nC\nD\nE\nE C 136.81\nD B 12.74\nC B 14.63\nB A 60.48\nA D 45.63\nA E 514.73\nA\nC";
+            
+            
 
             string message = messageEditor.Text;
             // Manage the input here => probably the function:
@@ -25,14 +25,38 @@
 
         private void onChangedQuestion(object sender, EventArgs e)
         {
+            string question1 = "The least number of train stations";
+            string question2 = "The shortest path to the destination station";
             if (questionNo == 1)
+
+            {
                 questionNo = 2;
+                headerLable.Text = "input: " + question2;
+            }
             else if (questionNo == 2)
+            {
                 questionNo = 1;
+                headerLable.Text = "input: " + question1;
+            }
             else
                 throw new ArgumentException("No sucha question");
+
+            // Clear the input box:
+            messageEditor.Text = "";
         }
-        
+
+        private void onPastedQuestion (object sender, EventArgs e)
+        {
+            string testCase1 = "4 2\nShiraz\nTehran\nIsfahan\nMashhad\nShiraz Tehran\nMashhad Isfahan\nMashhad";
+            string testCase2 = "5 4\nA\nB\nC\nD\nE\nE C 136.81\nD B 12.74\nC B 14.63\nB A 60.48\nA D 45.63\nA E 514.73\nA\nC";
+
+            if (questionNo == 1) messageEditor.Text = testCase1;
+            else if (questionNo == 2) messageEditor.Text = testCase2;
+            else throw new ArgumentException("unexcpected question no.");
+
+        }
+
+
         private void OnChangeThemeClicked(object sender, EventArgs e)
         {
             if (Application.Current.RequestedTheme == AppTheme.Light)
