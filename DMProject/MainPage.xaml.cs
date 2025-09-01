4e4107de12facc0e235181d30b20e5570daf09cf
AminEslamian <90086058+AminEslamian@users.noreply.github.com>
2025-09-01 10:33:12 +0330

    feat: add change question button and test cases

--- DMProject/MainPage.xaml.cs
+++ DMProject/MainPage.xaml.cs
@@ -3,6 +3,7 @@
     public partial class MainPage : ContentPage
     {
         int isOutputVisible; // for managing the output box visibility
+        int questionNo = 1; // for managing the question number
         public MainPage() 
 
         {
@@ -11,12 +12,25 @@
 
         private void OnSendClicked(object sender, EventArgs e)
         {
+            string testCase1 = "4 2\nShiraz\nTehran\nIsfahan\nMashhad\nShiraz Tehran\nMashhad Isfahan\nMashhad";
+            string testCase2 = "5 4\nA\nB\nC\nD\nE\nE C 136.81\nD B 12.74\nC B 14.63\nB A 60.48\nA D 45.63\nA E 514.73\nA\nC";
+
             string message = messageEditor.Text;
             // Manage the input here => probably the function:
             // --
             // display the second box:
             outputEditor.Text = "Output: " + message; // Just an example of output
 
+        }
+
+        private void onChangedQuestion(object sender, EventArgs e)
+        {
+            if (questionNo == 1)
+                questionNo = 2;
+            else if (questionNo == 2)
+                questionNo = 1;
+            else
+                throw new ArgumentException("No sucha question");
         }
         
         private void OnChangeThemeClicked(object sender, EventArgs e)
